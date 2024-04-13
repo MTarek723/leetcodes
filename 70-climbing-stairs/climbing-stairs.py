5aafc799f065fc3b823bf374d01028a6646a0d0e
Mahmoud Tarek <138780850+MTarek723@users.noreply.github.com>
2024-04-13 16:57:26 +0200

    Time: 11 ms (76.07%) | Memory: 11.5 MB (94.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def climbStairs(self, n):
-        memo = {}
-        return self.climb_stairs_helper(n, memo)
-
-    def climb_stairs_helper(self, n, memo):
-        if n == 0 or n == 1:
-            return 1
-        if n in memo:
-            return memo[n]
-        memo[n] = self.climb_stairs_helper(n-1, memo) + self.climb_stairs_helper(n-2, memo)
-        return memo[n]
+        """
+        :type n: int
+        :rtype: int
+        """
+        mem = [1,1,2]
+        for i in range(3,n+1):
+            mem.append(mem[i-2] + mem[i-1])
+        return mem[n]
+        
